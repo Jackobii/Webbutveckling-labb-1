46e5cbf18751eeb5b27e41042f0aa2c79d6501e5
Jackobii <jacob.fjellstrom@iths.se>
2023-02-06 23:05:35 +0100

    lite fix

---
+++ products.js
@@ -0,0 +1,18 @@
+let storeProducts = [
+    {
+        id: 1,
+        name: "Pure, Superduper Organic Cocoa Powder",
+        price: "250",
+        productinfo: "It's the purest you've ever seen...",
+        ingredients: "100% Pure Cacao",
+        images:  ["img/cacao-product-1-1.jpg", "img/cacao-product-1-2.jpg"]
+    },
+    {
+        id: 2,
+        name: "Lindt Lindor - Milk Chocolate Truffles",
+        price: "100",
+        productinfo: "Experience bliss with Lindt Lindor smooth melting milk chocolate truffles. The Lindt Master Chocolatiers combine expertise and the finest ingredients to produce the perfectly round milk chocolate shell with an irresistibly smooth melting filling. Since 1845, Lindt has been dedicated to producing the finest chocolates and the recipes created by the Lindt Master Chocolatiers are of the highest quality to delight your taste buds. The Lindor milk chocolate box is the ideal gift for every occasion, from Valentine's Day to Easter, birthdays and Christmas, as well as being perfect for indulging in a blissful moment all to yourself.",
+        ingredients: "100% Pure Cacao",
+        images:  ["img/cacao-product-2-1.webp"]
+    }
+];
