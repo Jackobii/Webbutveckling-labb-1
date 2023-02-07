d9d47d5ca705f00c196eff97baf41cd13519c5dd
Jackobii <jacob.fjellstrom@iths.se>
2023-02-07 20:59:32 +0100

    little fixes

--- site.js
+++ site.js
@@ -5,6 +5,14 @@ if (document.readyState == "loading") {
   }
 
 const shopProducts = [];
+const removeCartItemButtons = document.getElementsByClassName("btn-remove-cart-item");
+
+for(let i = 0; i < removeCartItemButtons.length; i++) {
+  const button = removeCartItemButtons[i];
+  button.addEventListener("click", function() {
+    console.log("clicked");
+  })
+}
 
 function ready () {
 
@@ -14,16 +22,15 @@ function ready () {
 function generateProductCards() {
   const shopProducts = document.querySelector(".shop-products"); 
   const productTemplate = document.getElementById("product-template");
-  const productCard = productTemplate.content.cloneNode(true);
 
   for (const item of storeProducts) {
     const shopProduct = productTemplate.content.cloneNode(true);
-    console.log(item);
 
     shopProduct.querySelector(".shop-item-image").src = item.images;
     shopProduct.querySelector(".shop-item-name").innerText = item.name;
     shopProduct.querySelector(".shop-item-productinfo").innerText = item.productinfo;
     shopProduct.querySelector(".shop-item-ingredients").innerText = `Innehåller: ${item.ingredients}`;
+    shopProduct.querySelector(".shop-item-id").innerText = `Id: ${item.id}`;
     shopProduct.querySelector(".shop-item-price").innerText = `${item.price} kr`;
 
     shopProducts.append(shopProduct)
